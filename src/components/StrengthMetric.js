b83e7de174d523d3321ea8d02f64ffa318a4aa5e
jah1603 <hendersonjamesalexander@gmail.com>
2018-09-17 22:02:47 +0100

    adds obeyedPercentage behaviour to strengthmetric

--- src/components/StrengthMetric.js
+++ src/components/StrengthMetric.js
@@ -4,6 +4,19 @@ import classNames from 'classnames';
 
 class StrengthMetric extends React.Component {
 
+  obeyedPercentage(){
+    let { principles, password } = this.props;
+
+    let obeyedCount = principles.map( p => p.predicate(password))
+                                .reduce((count, obeyed) =>
+                                  count + (satisfied ? 1 : 0),
+                                0 );
+
+    let principlesCount = principles.length;
+
+    return (obeyedCount / principlesCount ) * 100.0;
+  }
+
   principleObeyed(principle){
     let { password } = this.props;
 
