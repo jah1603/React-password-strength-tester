67cce3a22171f6a53d4d03353128c628f1d15161
jah1603 <hendersonjamesalexander@gmail.com>
2018-09-17 21:44:31 +0100

    passwordinput class created

--- src/components/PasswordInput.js
+++ src/components/PasswordInput.js
@@ -1,1 +1,27 @@
-class PasswordInput extends React.Component
+import React, { Component } from 'react';
+
+class PasswordInput extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { password: '' };
+  }
+
+  render () {
+
+    let { goodPasswordPrinciples } = this.props;
+
+    return (
+      <div class="container">
+        <div class="row"
+          <div class="col-md-8"
+            <PasswordField />
+          </div>
+          <div class="col-mid-4"
+            <StrengthMetric />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
