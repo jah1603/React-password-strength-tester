3a58725361a298d7436547b2365a12bef0e8f3ac
jah1603 <hendersonjamesalexander@gmail.com>
2018-09-17 22:34:37 +0100

    copies obeyedPercentage and progressColor into passwordfield component

--- src/components/PasswordField.js
+++ src/components/PasswordField.js
@@ -16,6 +16,29 @@ class PasswordField extends React.Component {
 
   }
 
+  obeyedPercentage(){
+    let { principles, password } = this.props;
+
+    let obeyedCount = principles.map( p => p.predicate(password))
+                                .reduce((count, obeyed) =>
+                                  count + (satisfied ? 1 : 0),
+                                0 );
+
+    let principlesCount = principles.length;
+
+    return (obeyedCount / principlesCount ) * 100.0;
+  }
+
+  progressColor(){
+    let percentage = this.obeyedPercentage();
+
+    return classNames({
+      danger: ( percetage < 33.4 ),
+      success: ( percentage >= 66.7 ),
+      warning: ( percentage >= 33.4 && percentage < 66.7 )
+    })
+  }
+
   render() {
     return (
       <Input type='password'
