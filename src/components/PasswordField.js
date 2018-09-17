99167b7ad1ee4b60bd37cfe114384e544042d50f
jah1603 <hendersonjamesalexander@gmail.com>
2018-09-17 22:28:08 +0100

    adds constructor to passwordfield

--- src/components/PasswordField.js
+++ src/components/PasswordField.js
@@ -4,6 +4,15 @@ import {Input} from 'react-bootstrap';
 
 class PasswordField extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+  }
+
+  handlePasswordChange(evt){
+    
+  }
+
   render() {
     return (
       <Input type='password'
